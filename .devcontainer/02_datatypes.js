e3bf5b6c15063f810ead2817056a78f66c85a4db
ajayprakash005 <ajayprakash1436@gmail.com>
2025-07-21 11:55:38 +0000

    datatypes

---
+++ .devcontainer/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";//treat all JS code as newr version
+
+//alert(3+3)// we are using nodejs,not browser
+
+console.log(3+3)
+//code readadditlty should be high
+console.log(ajay);
+
+
+let name ="ajay "
+let age =18
+let isLoggedIn=false
+
+//number=> 2to power 53
+//brognt
+//string=>""
+//boolean =>true/false
+//null=> standalone value
+//underfined=>
+//symbol=>unique
+
+//object
+
+console.log("typeof null")
