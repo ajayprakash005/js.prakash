91293bcfa8c595b05bd17dfceb1fe265ce489e6a
ajayprakash005 <ajayprakash1436@gmail.com>
2025-07-21 12:21:40 +0000

    convasion of opreton

---
+++ .devcontainer/01_basic/03_coconvarsionopretion.js
@@ -0,0 +1,28 @@
+let score = 33
+
+console.log("typeof score");
+console,log("typeof score");
+
+let valueINnnumber =Number(socre)
+console.log(typeof valueINnumber);
+console.log(valueINumber);
+
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1; false=>0
+let isLoggedIn =1
+
+let booleanIsLoogedIn =Boolean(isLoggedIn)
+console.log("booleanIsLoggedIn");
+
+//1=> true;0=> false
+//"=>false"
+//"ajay"=>true
+
+let someNumber=33
+
+let stringNumber =Strimg(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
